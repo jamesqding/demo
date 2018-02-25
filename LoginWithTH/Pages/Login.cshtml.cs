3d233bd8c328875ef92867b039f1ccdf83b85ff4
jamesqding@yahoo.com <jamesqding@yahoo.com>
2018-02-25 10:09:44 -0500

    fixed up add Tag Helper proj

---
+++ LoginWithTH/Pages/Login.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginWithTH.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public LoginViewModel ViewModel { get; set; }
+
+        public void OnGet()
+        {
+            ViewModel = new LoginViewModel() { UserName = "jding" };
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
